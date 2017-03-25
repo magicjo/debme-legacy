955ab12ab71894282e864c5a39b515051e961e62
magicjo <magicjo@tiglab.net>
2017-03-25 19:33:57 +0100

    feat(#3): foo_bar
     - cli foo_bar
    
    Tags: us-1, project, cli

--- src/debme.py
+++ src/debme.py
@@ -7,6 +7,9 @@ import debme
 if __name__ == '__main__':
     # Get args
     args = debme.cli()
+    # Case foo bar
+    if args['which'] == debme.PARSERS_ENUM['FOO_BAR']:
+        debme.foo_bar(args['foo_bar'])
     # Case setup
     if args['which'] == debme.PARSERS_ENUM['SETUP']:
         debme.ansible_run(['setup'], args)
